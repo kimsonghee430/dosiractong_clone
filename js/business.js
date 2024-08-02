b830fe6f886d097a12e44c24b2199cdc355aeac8
kimsonghee430 <bokchi8@gmail.com>
2024-08-02 15:59:01 +0900

    service , about 영역 완성. business 영역 js 적용중

---
+++ js/business.js
@@ -0,0 +1,14 @@
+window.addEventListener("load", function () {
+    const swBesiness = new Swiper(".sw-business", {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+      },
+    });
+  });
